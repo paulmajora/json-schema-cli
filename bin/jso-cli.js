b72f522a0e595a9a1ef81c08528d0a675ba10387
paulmajora <paulmajora@gmail.com>
2020-09-07 02:46:02 +0000

    ensure folder exists

--- bin/jso-cli.js
+++ bin/jso-cli.js
@@ -1,6 +1,6 @@
-
 const generateSchema = require('generate-schema');
 const fs = require('fs');
+const path = require('path');
 const optionDefinitions = [
     { name: 'output', alias: 'o', type: String },
     { name: 'data', alias: 'd', type: String },
@@ -10,5 +10,17 @@ const options = commandLineArgs(optionDefinitions);
 const validData = JSON.parse(
     fs.readFileSync(options.data, 'utf8')
 );
+const ensureDirectoryExistence = (filePath) => {
+  var dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+};
+
+
+
 const schema = generateSchema.json('Schema', validData);
+ensureDirectoryExistence(options.output);
 fs.writeFileSync(options.output, JSON.stringify(schema, null, 4));
