7819ec53a5ef25f7a0aa73c78ae0f4857cca3a3b
paulmajora <paulmajora@gmail.com>
2020-09-07 02:59:28 +0000

    v1.0.7

--- bin/jso-cli.js
+++ bin/jso-cli.js
@@ -1,16 +1,21 @@
+#! /usr/bin/env node
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
 var generateSchema = require('generate-schema');
+
 var optionDefinitions = [
     { name: 'output', alias: 'o', type: String },
-    { name: 'data', alias: 'd', type: String },
+    { name: 'data', alias: 'd', type: String }
 ];
 var commandLineArgs = require('command-line-args');
 var options = commandLineArgs(optionDefinitions);
 var validData = JSON.parse(
     fs.readFileSync(options.data, 'utf8')
 );
-var ensureDirectoryExistence = (filePath) => {
+
+function ensureDirectoryExistence(filePath) {
   var dirname = path.dirname(filePath);
   if (fs.existsSync(dirname)) {
     return true;
@@ -18,8 +23,6 @@ var ensureDirectoryExistence = (filePath) => {
   ensureDirectoryExistence(dirname);
   fs.mkdirSync(dirname);
 };
-
-
 
 var schema = generateSchema.json('Schema', validData);
 ensureDirectoryExistence(options.output);
